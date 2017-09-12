6ef9cb610f2f9b5b818d178fcc39cc55c41685af
Danilyk <Danilko_01@mail.ru>
2017-09-12 11:13:36 +0700

    fixTab

--- app/js/script.js
+++ app/js/script.js
@@ -3,13 +3,18 @@ $(document).ready(function(){
 	var btn = $('.b-btnMode__block');	
 	var List = $('.HowUseProgramm__block');
 
-    btn.click(function(e) {
-        e.preventDefault();
-        btn.removeClass('b-btnMode--active');
-        $(this).addClass('b-btnMode--active');
-        List.toggleClass('HowUseProgramm--notActive');
+    btn.click(function() {
+    	if (!$(this).hasClass('b-btnMode--active')){
+	        btn.removeClass('b-btnMode--active');
+	        $(this).addClass('b-btnMode--active');
+	        List.toggleClass('HowUseProgramm--notActive');
+		}
     });
-    
+    $('.fullModeLink').click(function(){
+    	btn.removeClass('b-btnMode--active');
+	    $('.b-btnMode__fullMode').addClass('b-btnMode--active');
+    	List.toggleClass('HowUseProgramm--notActive');
+    })
 });
 
 
